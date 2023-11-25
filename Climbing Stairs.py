cda7ddac2b0955a26674812a06d02786fbf572b4
lebedev2k <lebedev2k@mail.ru>
2023-11-25 08:43:00 +0700

    solved climbing stairs

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -1,24 +1,16 @@
 from math import factorial
 
 def climbStairs(n: int) -> int:
-    if n == 1:
-        return 1
+    n2 = n//2
+    n1 = n%2
+    ways = 1
 
-    if n%2 == 1:
-        n2 = n//2
-        n1 = 1
-        ways = factorial(n1 + n2)
-    else:
-        n2 = n//2
-        n1 = 0
-        ways = 1
-    nfact = factorial(n)
-    while n2 != 1:
+    while n2 != 0:
+        ways += factorial(n1+n2)/(factorial(n1)*factorial(n2))
         n2 -= 1
         n1 += 2
-        # ways += factorial(n1+n2)
-        ways += nfact/(factorial(n1)*factorial(n2))
-    return ways+1
+
+    return int(ways)
 
 assert(climbStairs(2)==2)
 assert(climbStairs(1)==1)
